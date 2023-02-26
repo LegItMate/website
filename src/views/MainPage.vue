ad8d85d1dfe5e56825ae45ec7949ad6e2179dbdd
LegItMate <77503777+LegItMate@users.noreply.github.com>
2023-02-26 22:59:37 +0530

    paragraphs added

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -20,17 +20,17 @@
     </div>
 
     <div class="lightweight" id="lightweight">
-      <p class="lightweight_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita similique, nihil voluptas unde ipsum repellat vitae? Mollitia assumenda animi veniam nesciunt neque sit? Nesciunt impedit suscipit ratione ipsam voluptate!</p>
+      <p class="lightweight_text">Lazap is a lightweight, cross-platform game launcher designed to provide seamless experience to gamers. Alongside being lightweight, Lazap has a minimalistic, simplistic and modernistic look. Lazap's user-friendly design makes it run smoothly and efficiently on any device. With Lazap, gamers can enjoy a minimalistic and modernistic user interface, cross-platform compatibility, and a lightweight design that ensures optimal performance.</p>
       <img class="image" src="https://source.unsplash.com/random">
     </div>
 
     <div class="cross_platform" id="cross_platform">
       <img class="image" src="https://source.unsplash.com/random">
-      <p class="cross_platform_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita similique, nihil voluptas unde ipsum repellat vitae? Mollitia assumenda animi veniam nesciunt neque sit? Nesciunt impedit suscipit ratione ipsam voluptate!</p>
+      <p class="cross_platform_text">As a cross-platform game launcher, Lazap is compatible with Windows as well as Linux. Lazap's cross-platform compatibility does not hinder its performance or its lightweight experience and tries to maintain the same performance in both Windows and Linux.</p>
     </div>
 
     <div class="game_launcher" id="game_launcher">
-      <p class="game_launcher_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit expedita similique, nihil voluptas unde ipsum repellat vitae? Mollitia assumenda animi veniam nesciunt neque sit? Nesciunt impedit suscipit ratione ipsam voluptate!</p>
+      <p class="game_launcher_text">With Lazap, gamers can easily access their games that have been installed on their computer from a singular place without the hassle of loading different game launchers before deciding what to game. The app consists of a glancing and modernist UI which makes it easier for users to interact. Lazap also provides a range of customisations, such as accent color and the ability to add custom games which are not automatically detected by the launcher.</p>
       <img class="image" src="https://source.unsplash.com/random">
     </div>
   </div>
